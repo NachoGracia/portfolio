54b2d961c51610cb60ce922ea7097f4108e4ffc0
NachoGracia <gracianacho@gmail.com>
2024-04-16 12:05:02 +0200

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <></>;
-}
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default App;
