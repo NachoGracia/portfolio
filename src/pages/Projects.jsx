0c2b650ed26225d1b3be2786db327bf85fb7d459
Nacho <nacho@MacBook-Pro-de-Nacho.local>
2024-04-28 22:57:46 +0200

    update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCards
-              imgPath="../public/oyeVecino.png"
+              imgPath="https://res.cloudinary.com/diyouj1se/image/upload/v1714337637/portfolio/j6tcfn9zpbwykmvkrpre.png"
               isBlog={false}
               title="oyeVecino frontend"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
@@ -33,7 +33,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCards
-              imgPath="../public/oyeVecino.png"
+              imgPath="https://res.cloudinary.com/diyouj1se/image/upload/v1714337637/portfolio/j6tcfn9zpbwykmvkrpre.png"
               isBlog={false}
               title="oyeVecino backend"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
@@ -44,7 +44,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCards
-              imgPath="../public/hubJuegos.png"
+              imgPath="https://res.cloudinary.com/diyouj1se/image/upload/v1714337649/portfolio/gsp1qvx6u37anpil9vsu.png"
               isBlog={false}
               title="Hub de juegos"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
