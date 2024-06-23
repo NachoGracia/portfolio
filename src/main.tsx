2c5fe4be801668327d2f5203e1008d567144bc99
Nacho Gracia <gracianacho@gmail.com>
2024-06-23 15:31:36 +0200

    updated for TS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 //import "./main.css";
 
-import { router } from "./routes/routes.jsx";
+import { router } from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
