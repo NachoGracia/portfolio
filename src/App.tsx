0f94fd0f0d84a3b318f7a72b5301fa3372f5bf4c
Nacho Gracia <gracianacho@gmail.com>
2024-06-23 15:30:17 +0200

    updated to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import "./style.css";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <>
       <NavBar />
